6ef309fdc67c7d182067aea3a7ca5c2dc8adeda3
Pavel Zarubin <paha996@yandex.ru>
2024-02-26 17:58:11 +0300

    test gzip

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"bytes"
+	"compress/gzip"
 	"context"
 	"fmt"
 	"github.com/segmentio/kafka-go"
@@ -38,9 +40,32 @@ func (h *Handler) Handle(ctx *fasthttp.RequestCtx) {
 		i := 0
 		for {
 			go func() {
+				str := `
+{
+        "Id": "102",
+        "Complaints": "180",
+        "Request": null,
+        "JRFile": "{\"$type\":\"System.Collections.Generic.Dictionary2[[System.String, mscorlib],[System.String, mscorlib]], mscorlib\",\"PublicKey\":\"0e4bcafa-5201-4636-94e4-6e6b71d74937\",\"Name\":\"a6d5e339768690fac1ca0b5eb4c4bfb7\",\"Extension\":\"jpg\",\"MimeType\":\"image/jpeg\",\"Size\":\"37146\"}",
+					"Complaint": null,
+					"CreateBy": "10",
+					"UpdateBy": "10",
+					"CustObjVersion": "7354",
+					"CreateDate": "2024-01-12T09:21:55.82624Z",
+					"UpdateDate": "2024-01-12T09:21:55.82624Z"
+				}
+`
+				var b bytes.Buffer
+				gz := gzip.NewWriter(&b)
+				if _, err := gz.Write([]byte(str)); err != nil {
+					log.Fatal(err)
+				}
+				if err := gz.Close(); err != nil {
+					log.Fatal(err)
+				}
+
 				err := h.kafka.WriteMessages(context.Background(), kafka.Message{
-					Key:   []byte("test"),
-					Value: []byte("test"),
+					Key:   []byte("52a44ea8-f2e0-4321-a710-fbfce7c09db9"),
+					Value: b.Bytes(),
 				})
 				if err != nil {
 					panic(err)
